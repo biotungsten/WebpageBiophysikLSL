ba4d0dd48d27e97d9dfa96b6dc3994ef764bebd3
David Sauer <sauerdavid13@icloud.com>
2020-09-01 18:31:03 +0200

    added protein

--- Particle.js
+++ Particle.js
@@ -1,10 +1,16 @@
 class Particle {
-    constructor(x, y, col, dimx, dimy, interactions=[]) {
+    constructor(x, y, col, dimx, dimy, protein=false, interactions=[]) {
         this.pos = {x: x, y: y};
         this.vel = {x: 0, y: 0};
         this.col = col;
         this.interactions = interactions;
         this.dim = {x: dimx, y: dimy};
+        this.protein = protein;
+        this.low_diff = 1;
+        if (protein == true){
+            this.low_diff = 5;
+        }
+        
     }
 
     applyForce(force) {
@@ -33,15 +39,47 @@ class Particle {
         this.applyForce(diffusor);
         return diffusor
     }
+    
+    updateDiff(){
+        if (this.low_diff > 1) {
+            this.low_diff -= 0.2;
+        } else if (this.low_diff < 1) {
+            this.low_diff = 1;
+        }
+    }
 
-    updateParticle(func, arg, draw=true) {
-        let localScale = 1;
-        let diffusor = this.diffuse(localScale)
+    distanceTo(x,y){
+        let distX = Math.abs(this.pos.x-x);
+        let distY = Math.abs(this.pos.y-y);
+        return Math.sqrt(distX*distX+distY*distY);
+    }
+
+    updateParticle(func, arg, particles, draw=true) {
+        if (this.protein == true) {
+            for (let idx = 0; idx < particles.length; idx++) {
+                var element = particles[idx];
+                if (this.distanceTo(element.pos.x, element.pos.y) < 20){
+                    if (element.protein == false){
+                        particles[idx].set_low_diff(21-this.distanceTo(element.pos.x, element.pos.y));
+                    }
+                }
+            }
+        } 
+        var localScale = 1/this.low_diff;
+        if (localScale < 0.15){
+            localScale = 0.15
+        }
+        this.updateDiff();
+        let diffusor = this.diffuse(localScale);
         this.updatePosition();
         if (draw == true) {
-            this.drawParticle(func, arg)
+            this.drawParticle(func, arg);
         }
-        this.applyForce({x: -diffusor.x, y: -diffusor.y})
+        this.applyForce({x: -diffusor.x, y: -diffusor.y});
+    }
+
+    set_low_diff(x) {
+        this.low_diff = x;
     }
 
     drawParticle(func, arg) {
--- sketch.js
+++ sketch.js
@@ -8,9 +8,26 @@ let canvas1 = function(p) {
     let dimy = 400;
 
     drawer = function(p, th){
-        p.fill(th.col);
-        p.circle(th.pos.x, th.pos.y, 4);
+        if (th.protein == true) {
+            p.fill("black");
+            polygon(th.x, th.y, 10, 7);
+        } else {
+            p.fill(th.col);
+            p.circle(th.pos.x, th.pos.y, 4);
+        }
     }
+
+    polygon = function(x, y, radius, npoints) {
+        let angle = 6.28 / npoints;
+        p.beginShape();
+        for (let a = 0; a < 6.28; a += angle) {
+          let sx = x + Math.cos(a) * radius;
+          let sy = y + Math.sin(a) * radius;
+          p.vertex(sx, sy);
+        }
+        p.endShape(p.CLOSE);
+      }
+      
 
     p.setup = function(){
         var cnv = p.createCanvas(dimx, dimy);
@@ -29,18 +46,62 @@ let canvas1 = function(p) {
         p.noStroke();
         for (let idx = 0; idx < particles.length; idx++) {
             const particle = particles[idx];
-            particle.updateParticle(drawer, p);
-    }
+            particle.updateParticle(drawer, p, particles);
+        }
     }
 }
 
 let canvas2 = function(p) {
+    var particles = [];
+    let noOfParticles = 1000;
+    let colors = ["red", "green", "yellow", "blue", "pink"]
+    let typeCoordinates = [{x: 50, y: 50}, {x: 150, y: 150}, {x: 250, y: 250}, {x: 300, y: 300}, {x: 350, y: 350}] 
+    let noOfParticleTypes = 3;
+    let dimx = 400;
+    let dimy = 400;
+
+    drawer = function(p, th){
+        if (th.protein == true) {
+            p.fill("black");
+            polygon(th.pos.x, th.pos.y, 10, 7);
+        } else {
+            p.fill(th.col);
+            p.circle(th.pos.x, th.pos.y, 4);
+        }
+    }
+
+    polygon = function(x, y, radius, npoints) {
+        let angle = 6.28 / npoints;
+        p.beginShape();
+        for (let a = 0; a < 6.28; a += angle) {
+          let sx = x + Math.cos(a) * radius;
+          let sy = y + Math.sin(a) * radius;
+          p.vertex(sx, sy);
+        }
+        p.endShape(p.CLOSE);
+      }
+      
+
     p.setup = function(){
-        var cnv = p.createCanvas(400, 400);
+        var cnv = p.createCanvas(dimx, dimy);
+
+        for (let i = 0; i < noOfParticleTypes; i++) {
+            var color = colors[i];
+            for (let j = 0; j < Math.floor(noOfParticles/noOfParticleTypes); j++) {
+                particles[Math.floor(i*noOfParticles/noOfParticleTypes)+j] = new Particle(typeCoordinates[i].x, typeCoordinates[i].y, color, dimx, dimy)
+            }
+            
+        }
+        particles[particles.length] = new Particle(200, 200, "NA", dimx, dimy, true);
     }
 
     p.draw = function(){
-        p.background(100);
+        p.background(220);
+        p.noStroke();
+        for (let idx = 0; idx < particles.length; idx++) {
+            const particle = particles[idx];
+            particle.updateParticle(drawer, p, particles);
+        }
     }
 }
 
